f323445ad629d0f0ce23d5d3c538a7e954b41c77
ninovsnino <ninovsnino@gmail.com>
2016-05-21 23:02:55 +0800

    Add basic tests via manage test command

--- manage.py
+++ manage.py
@@ -8,5 +8,12 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 
+@manager.command
+def test():
+    """ unit tests """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == '__main__':
     manager.run()
